e8cea36c4d7e3657531482ff02df51c4cda18f92
erik@ejnelson.com <christamichels@Christas-MacBook-Pro.local>
2017-01-27 15:32:46 -0600

    end of time

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,168 @@
+
 $(function(){
-  console.log('JQ here');
+  // console.log('JQ here');
+
+
+
+  getPets();
+  getOwner();
+  getStatus();
+  $('#ownerReg').on('submit', regOwner);
+
+
+  $('#addPet').on('submit',regPet);
+  $('#petTable').on('click', '.update', updatePet);
+  $('#petTable').on('click', '.delete', deletePet);
+  $('#petTable').on('click', '.checkIn', changeStatus);
 });
+
+function regOwner(event){
+  event.preventDefault();
+  var newOwner = $(this).serialize();
+  console.log('new Owner:', newOwner);
+  $.ajax({
+    url:'/router/owners',
+    type:'POST',
+    data: newOwner,
+    success: getOwner
+  })
+}
+
+function getOwner(){
+  $.ajax({
+    url: '/router/owners',
+    type: 'GET',
+    success: showOwner
+
+
+  })
+}
+
+function showOwner(owners){
+  console.log(owners);
+  $('#select').empty();
+  owners.forEach(function(owner){
+    var $owner = $('<option>' + owner.first_name + " " + owner.last_name + '</option>');
+    $owner.data("id", owner.id);
+    $('#select').append($owner);
+  });
+}
+function regPet(event){
+  event.preventDefault();
+  var newPet = $(this).serialize();
+  var ownerId = $("select option:selected").data('id');
+  newPet += '&id=' + ownerId;
+  // console.log(newPet);
+  $.ajax({
+    url: '/router/pet',
+    type: 'POST',
+    data: newPet,
+    success: getPets
+  });
+
+}
+
+function getPets(pets){
+  // console.log('pets');
+  $.ajax({
+    url: '/router/pet',
+    type: 'GET',
+    success: showPets
+  });
+}
+function showPets(pets){
+  // console.log('asdfpets');
+  $('#petTable').empty();
+  pets.forEach(function(pet){
+    // console.log(pet.name+' '+pet.id);
+    var $pet = $('<form class="coolform"></form>');
+    var $petOwner = $('<td>' + pet.first_name + " " + pet.last_name +'</td>');
+    var $petName = $('<td><input type = "text" name = petName value = "' + pet.name + '"/></td>');
+    var $petBreed = $('<td><input type = "text" name = petBreed value = "' + pet.breed + '"/></td>');
+    var $petColor = $('<td><input type = "text" name = petColor value = "' + pet.color + '"</td>');
+    var $petUpdate = $('<td>' +'<button id="'+pet.id+'" class="btn btn-default update">Update</button>' + '</td>');
+    var $petDelete = $('<td>' + '<button id="'+pet.id+'"  class="btn btn-default delete">Delete</button>'+ '</td>');
+    var $petCheckIn_CheckOut = $('<td>' + '<button data-id='+pet.id+' id="update'+pet.id+'" class="btn btn-default checkIn">Check In</button>'+ '</td>');
+    $pet.append($petOwner);
+    $pet.append($petName);
+    $pet.append($petBreed);
+    $pet.append($petColor);
+    $pet.append($petUpdate);
+    $pet.append($petDelete);
+    $pet.append($petCheckIn_CheckOut);
+
+
+    $('#petTable').append($pet);
+  });
+}
+
+function updatePet(event){
+  event.preventDefault();
+  var $updateButton = $(this);
+
+  var $form = $updateButton.closest('form');
+  var data = $form.serialize();
+  // console.log(data);
+  $.ajax({
+    url: 'router/pet/'+$updateButton.attr('id'),
+    type: 'PUT',
+    data: data,
+    success: getPets
+  });
+}
+
+function deletePet(event) {
+  event.preventDefault();
+
+
+  $.ajax({
+    url: 'router/pet/' + $(this).attr('id'),
+    type: 'DELETE',
+    success: getPets
+  });
+
+}
+
+function getStatus(){
+  $.ajax({
+    url: '/router/visits',
+    type: 'GET',
+    success: showStatus
+  });
+}
+
+function showStatus(stata) {
+  stata.forEach(function(statusObj){
+    var text = "";
+    var status = {};
+    if(statusObj.check_in_date == null){
+      text = "Check In";
+      status.state = "in";
+    } else if (statusObj.check_out_date == null) {
+      text = "Check Out";
+      status.state = "out";
+    } else {
+      text = "Check Out";
+      status.state = "out";
+    }
+
+    $("#update"+statusObj.pets_id).data('status', status);
+
+    $("#update"+statusObj.pets_id).html(text);
+    console.log(text);
+    console.log(statusObj.pets_id);
+    console.log(statusObj.check_out_date);
+    console.log(statusObj.check_in_date);
+  });
+}
+
+function changeStatus(event) {
+  event.preventDefault();
+
+  $.ajax({
+    url: 'router/visits/' + $(this).attr('id'),
+    type: 'PUT',
+    data: $(this).data('status');
+    success: getStatus
+  });
+}
--- routes/router.js
+++ routes/router.js
@@ -9,7 +9,37 @@ var config = { database: "upsilon" };
 // think of as 'how I connect to DB'
 var pool = new pg.Pool(config);
 
-router.get("/", function(req, res) {
+// router.get("/", function(req, res) {
+//   // err - an error object, will be non-null if there was some error
+//   //       connecting to the DB. ex. DB not running, config is incorrect
+//   // client - used to make actual queries against DB
+//   // done - function to call when we are finished
+//   //        returns the connection back to the pool
+//   pool.connect(function(err, client, done) {
+//     if (err) {
+//       console.log("Error connecting to DB", err);
+//       res.sendStatus(500);
+//       done();
+//     } else {
+//       // no error occurred, time to query
+//       // 1. sql string - the actual SQL query we want to running
+//       // 2. callback - function to run after the database gives us our result
+//       //               takes an error object and the result object as it's args
+//       client.query("SELECT * FROM pets JOIN owners ON pets.owners_id = owners.id JOIN visits ON pets.id = visits.pets_id", function(err, result) {
+//         done();
+//         if (err) {
+//           console.log("Error querying DB", err);
+//           res.sendStatus(500);
+//         } else {
+//           console.log("Got info from DB", result.rows);
+//           res.send(result.rows);
+//         }
+//       });
+//     }
+//   });
+// });
+
+router.get("/owners", function(req, res) {
   // err - an error object, will be non-null if there was some error
   //       connecting to the DB. ex. DB not running, config is incorrect
   // client - used to make actual queries against DB
@@ -25,7 +55,7 @@ router.get("/", function(req, res) {
       // 1. sql string - the actual SQL query we want to running
       // 2. callback - function to run after the database gives us our result
       //               takes an error object and the result object as it's args
-      client.query("SELECT * FROM pets JOIN owners ON pets.owners_id = owners.id JOIN visits ON pets.id = visits.pets_id", function(err, result) {
+      client.query("SELECT id,first_name,last_name FROM owners ", function(err, result) {
         done();
         if (err) {
           console.log("Error querying DB", err);
@@ -39,7 +69,7 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/", function(req, res) {
+router.post("/owners", function(req, res) {
   // err - an error object, will be non-null if there was some error
   //       connecting to the DB. ex. DB not running, config is incorrect
   // client - used to make actual queries against DB
@@ -58,7 +88,7 @@ router.post("/", function(req, res) {
       //               takes an error object and the result object as it's args
       client.query(
         "INSERT INTO owners (first_name,last_name) VALUES ($1, $2) RETURNING *;",
-        [ req.body.first_name, req.body.last_name],
+        [ req.body.firstName, req.body.lastName],
         function(err, result) {
           done();
           if (err) {
@@ -70,50 +100,153 @@ router.post("/", function(req, res) {
           }
         }
       );
-      client.query(
-        "INSERT INTO pets (name, breed, color, owners_id) VALUES ($1, $2, $3, $4) RETURNING *;",
-        [ req.body.name, req.body.breed, req.body.color, req.body.owners_id],
-        function(err, result) {
-          done();
-          if (err) {
-            console.log("Error querying DB", err);
-            res.sendStatus(500);
-          } else {
-            console.log("Got info from DB", result.rows);
-            res.send(result.rows);
-          }
-        }
-      );
-      client.query(
-        "INSERT INTO visits (check_in_date, check_out_date, pets_id) VALUES ($1, $2, $3) RETURNING *;",
-        [ req.body.check_in_date, req.body.check_out_date, req.body.pets_id],
-        function(err, result) {
-          done();
-          if (err) {
-            console.log("Error querying DB", err);
-            res.sendStatus(500);
-          } else {
-            console.log("Got info from DB", result.rows);
-            res.send(result.rows);
-          }
-        }
-      );
+      // client.query(
+      //   "INSERT INTO pets (name, breed, color, owners_id) VALUES ($1, $2, $3, $4) RETURNING *;",
+      //   [ req.body.name, req.body.breed, req.body.color, req.body.owners_id],
+      //   function(err, result) {
+      //     done();
+      //     if (err) {
+      //       console.log("Error querying DB", err);
+      //       res.sendStatus(500);
+      //     } else {
+      //       console.log("Got info from DB", result.rows);
+      //       res.send(result.rows);
+      //     }
+      //   }
+      // );
+      // client.query(
+      //   "INSERT INTO visits (check_in_date, check_out_date, pets_id) VALUES ($1, $2, $3) RETURNING *;",
+      //   [ req.body.check_in_date, req.body.check_out_date, req.body.pets_id],
+      //   function(err, result) {
+      //     done();
+      //     if (err) {
+      //       console.log("Error querying DB", err);
+      //       res.sendStatus(500);
+      //     } else {
+      //       console.log("Got info from DB", result.rows);
+      //       res.send(result.rows);
+      //     }
+      //   }
+      // );
     }
   });
 });
 
+
+router.post("/pet", function(req, res) {
+  // err - an error object, will be non-null if there was some error
+  //       connecting to the DB. ex. DB not running, config is incorrect
+  // client - used to make actual queries against DB
+  // done - function to call when we are finished
+  //        returns the connection back to the pool
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("Error connecting to DB", err);
+      res.sendStatus(500);
+      done();
+    } else {
+      // no error occurred, time to query
+      // 1. sql string - the actual SQL query we want to running
+      // 2. array of data - any data we want to pass to a parameterized statement
+      // 3. callback - function to run after the database gives us our result
+      //               takes an error object and the result object as it's args
+
+      client.query(
+        "INSERT INTO pets (name, breed, color, owners_id) VALUES ($1, $2, $3, $4) RETURNING *;",
+        [ req.body.petName, req.body.breed, req.body.color, req.body.id],
+        function(err, result) {
+          done();
+          if (err) {
+            console.log("Error querying DB", err);
+            res.sendStatus(500);
+          } else {
+            console.log("Got info from DB", result.rows);
+            res.send(result.rows);
+          }
+        }
+      );
+
+    }
+  });
+});
+
+
+router.get("/pet", function(req, res) {
+  // err - an error object, will be non-null if there was some error
+  //       connecting to the DB. ex. DB not running, config is incorrect
+  // client - used to make actual queries against DB
+  // done - function to call when we are finished
+  //        returns the connection back to the pool
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("Error connecting to DB", err);
+      res.sendStatus(500);
+      done();
+    } else {
+      // no error occurred, time to query
+      // 1. sql string - the actual SQL query we want to running
+      // 2. callback - function to run after the database gives us our result
+      //               takes an error object and the result object as it's args
+      client.query("SELECT * FROM owners RIGHT JOIN pets ON pets.owners_id=owners.id", function(err, result) {
+        done();
+        if (err) {
+          console.log("Error querying DB", err);
+          res.sendStatus(500);
+        } else {
+          console.log("Got info from DB", result.rows);
+          res.send(result.rows);
+        }
+      });
+    }
+  });
+});
+
+router.get("/visits", function(req, res) {
+  // err - an error object, will be non-null if there was some error
+  //       connecting to the DB. ex. DB not running, config is incorrect
+  // client - used to make actual queries against DB
+  // done - function to call when we are finished
+  //        returns the connection back to the pool
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("Error connecting to DB", err);
+      res.sendStatus(500);
+      done();
+    } else {
+      // no error occurred, time to query
+      // 1. sql string - the actual SQL query we want to running
+      // 2. callback - function to run after the database gives us our result
+      //               takes an error object and the result object as it's args
+      client.query("SELECT * FROM visits", function(err, result) {
+        done();
+        if (err) {
+          console.log("Error querying DB", err);
+          res.sendStatus(500);
+        } else {
+          console.log("Got info from DB", result.rows);
+          res.send(result.rows);
+        }
+      });
+    }
+  });
+});
+
+
 //localhost:3000/books/4
 //req.params.id ==='4'
-router.put("/:id",function(req,res){
-  console.log('put')
+router.put("/pet/:id",function(req,res){
+  console.log(req.params.id);
+  console.log(req.body.petName);
+  console.log(req.body.petBreed);
+  console.log(req.body.petColor);
   pool.connect(function(err,client,done){
       if (err){
         console.log('error connecting to DB',err);
         res.sendStatus(500);
         done();
       }else{
-        client.query('UPDATE books SET title=$2,author=$3,publication_date=$4,edition=$5,publisher=$6 WHERE id=$1 RETURNING *',
-                      [req.params.id,req.body.title,req.body.author,req.body.published,req.body.edition,req.body.publisher],
+        client.query('UPDATE pets SET name=$2,breed=$3,color=$4 WHERE id=$1 RETURNING *',
+                      [req.params.id,req.body.petName,req.body.petBreed,req.body.petColor],
                       function(err,result){
                         if (err){
                           console.log('error updating book');
@@ -132,18 +265,49 @@ router.put("/:id",function(req,res){
 
 });
 
-router.delete("/:id",function(req,res){
+
+router.put("/visits/:id",function(req,res){
+  console.log(req.body.state);
   pool.connect(function(err,client,done){
       if (err){
         console.log('error connecting to DB',err);
         res.sendStatus(500);
         done();
       }else{
-        client.query('DELETE FROM books WHERE id=$1',
+        client.query('UPDATE pets SET name=$2,breed=$3,color=$4 WHERE id=$1 RETURNING *',
+                      [req.params.id,req.body.petName,req.body.petBreed,req.body.petColor],
+                      function(err,result){
+                        if (err){
+                          console.log('error updating book');
+                          res.sendStatus(500);
+
+                        }else {
+
+                          res.send(result.rows);
+                        }
+                        done();
+                      }
+                    )
+      }
+
+  });
+
+});
+
+
+router.delete("/pet/:id",function(req,res){
+  pool.connect(function(err,client,done){
+      if (err){
+        console.log('error connecting to DB',err);
+        res.sendStatus(500);
+        done();
+      }else{
+
+        client.query('DELETE FROM pets WHERE id=$1;',
                       [req.params.id],
                       function(err,result){
                         if (err){
-                          console.log('error deleting book');
+                          console.log('error deleting pet');
                           res.sendStatus(500);
 
                         }else {
